dd3edb6505fd1e0e436bc35f4c4e46e1c9807a6a
Tyron Gower <ty@companionsoftware.com.au>
2021-06-16 11:31:26 +1000

    initial commit

---
+++ happy-little-helpers/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace happy_little_helpers
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime ToEndOfYear(this DateTime dateTime, bool keepTime = false, CultureInfo cultureInfo = null)
+        {
+
+            var culture = cultureInfo ?? CultureInfo.CurrentCulture;
+            var month = culture.Calendar.GetMonthsInYear(dateTime.Year);
+            var day = culture.Calendar.GetDaysInMonth(dateTime.Year,month);
+            var hour = keepTime ? dateTime.Hour : 0;
+            var minute = keepTime ? dateTime.Minute : 0;
+            var seconds = keepTime ? dateTime.Second : 0;
+ 
+            return new DateTime(dateTime.Year, month, day, hour, minute, seconds);
+        }
+    }
+}
